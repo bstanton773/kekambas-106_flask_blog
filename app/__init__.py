162d23bf2c701ab9773efe4cda804c641946eee1
Brian <bstanton773@gmail.com>
2023-01-25 16:00:11 -0600

    Add Flask-CORS to our app

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_cors import CORS
 # Import the Config class from the config module - will have all of the app's configurations
 from config import Config
 
@@ -11,6 +12,9 @@ from config import Config
 app = Flask(__name__)
 # Configure the app using the Config class
 app.config.from_object(Config)
+
+# Allow CORS for all origins
+CORS(app)
 
 # Create an instance of SQLAlchemy to represent our database
 db = SQLAlchemy(app)
