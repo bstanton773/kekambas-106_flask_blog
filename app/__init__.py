8d9c7431a4c63d38cd6e54d753542da57bf7973b
Brian <bstanton773@gmail.com>
2023-01-25 14:43:16 -0600

    Add api blueprint with one route

--- app/__init__.py
+++ app/__init__.py
@@ -23,6 +23,9 @@ login.login_view = 'login'
 login.login_message = 'You must be logged in to perform this action'
 login.login_message_category = 'danger'
 
+# Import the api blueprint and register it with the Flask Application
+from app.blueprints.api import api
+app.register_blueprint(api)
 
 # import all of the routes from the routes file into the current folder
 from . import routes, models
